37a2a384faa712748e0ebdde520088377ecbf139
farshad <far3farazdel@gmail.com>
2024-03-02 13:33:29 +0330

    new setting

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ import path from "path"
 
 
 const app = express()
-app.set('trust proxy', true) // will return real ip address even behind proxy
+// app.set('trust proxy', true) // will return real ip address even behind proxy
 
 
 app.use("/static", express.static("public", { "maxage": "2h" }))
 
 app.get('/', (req, res) => {
     let ip1 = req.headers['x-forwarded-for'] //if there is more than one ip address 
-    //complete format --> req.headers['x-forwarded-for']?.split(',').shift()
+    const multiip = req.headers['x-forwarded-for']?.split(',').shift()
     let ip2 = req.socket.remoteAddress
     res.header("Cache-Control", "public max-age=86400")
     res.header("Content-Type", "text/html")
-    res.send(`<h1>req.headers['x-forwarded-for'] ${ip1} </h1><h1>req.socket.remoteAddress ${ip2}</h1>`);
+    res.send(`<h1>req.headers['x-forwarded-for'] ${ip1} </h1><h1>req.socket.remoteAddress ${ip2}</h1><h1>multi ip if exists: ${multiip}</h1>`);
 })
 
 app.listen(3000, () => {
